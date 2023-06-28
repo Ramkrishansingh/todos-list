2d9c64632d542e0707195ddd0190947b6b412baa
Ramkrishansingh <130742517+Ramkrishansingh@users.noreply.github.com>
2023-06-28 15:34:42 +0300

    Initial commit

---
+++ src/MyComponents/AddTodo.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+export const AddTodo = ({addTodo}) => {
+    const [title, setTitle] = useState('');
+    const [desc, setDesc] = useState('');
+
+    const submit = (e) => {
+        e.preventDefault();
+        if(!title || !desc){
+            alert('Title or Description can not be empty');
+        }
+        addTodo(title, desc);
+    }
+    return (
+        <div className='container'>
+            <h3 className='my-3'>Add your Todo</h3>
+            <form onSubmit={submit}>
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label">Todo Titile</label>
+                    <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} className="form-control" id="title" aria-describedby="emailHelp" />
+                    
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="desc" className="form-label">Todo Description</label>
+                    <input type="text" value={desc} onChange={(e)=>{setDesc(e.target.value)}} className="form-control" id="desc" />
+                </div>
+                <button type="submit" className="btn btn-sm btn-success">Add</button>
+            </form>
+        </div>
+    )
+}
